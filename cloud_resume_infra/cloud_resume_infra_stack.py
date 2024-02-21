fa9aeb4e8b386cd344e754c84e5c0f5e315aaf7f
toogoodyshoes <lekhadenihar11@gmail.com>
2024-02-21 17:23:33 +0530

    Summary 1. Created a hosted zone named 'lekhadenihar.dev' for the static website.

--- cloud_resume_infra/cloud_resume_infra_stack.py
+++ cloud_resume_infra/cloud_resume_infra_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     Stack,
     aws_s3 as s3,
     aws_iam as iam,
+    aws_route53 as r53,
 )
 from constructs import Construct
 
@@ -10,6 +11,7 @@ class CloudResumeInfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Create a S3 bucket to host static website.
         resume_bucket = s3.Bucket(
             self, 
             "cri", 
@@ -19,10 +21,18 @@ class CloudResumeInfraStack(Stack):
             block_public_access=s3.BlockPublicAccess(restrict_public_buckets=False)
         )
 
+        # Attach a bucket policy to allow public access.
         resume_bucket.add_to_resource_policy(
             permission=iam.PolicyStatement(
                 actions=['s3:GetObject'],
                 principals=[iam.StarPrincipal()],
                 resources=[f'{resume_bucket.bucket_arn}/*'],
             )
+        )
+
+        # Create a hosted zone for domain.
+        r53.PublicHostedZone(
+            self, 
+            "static-resume-hz", 
+            zone_name="lekhadenihar.dev",
         )
